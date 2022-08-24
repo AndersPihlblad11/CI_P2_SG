ae97824600d42f851684886ace357695d022b35e
Anders Pihlblad <anderspihlblad1998@gmail.com>
2022-08-24 13:11:53 +0000

    touching up js code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var level = 0;
 
 $(document).keypress(function() {
     if (!started) {
-      $("#level-title").text("Level " + level);
+      $("#title").text("Level " + level);
       nextSequence();
       started = true;
     }
@@ -16,55 +16,53 @@ $(document).keypress(function() {
 
 $(".btn").click(function() {
  
-    let userChosenColour = $(this).attr("id");
-
+    let userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
 
-    playSound(userChosenColour);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+
+    checkAnswer(userClickedPattern.length-1);
   
   });
 
   function checkAnswer(currentLevel) {
 
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-
-      console.log("success");
-
       if (userClickedPattern.length === gamePattern.length){
         setTimeout(function () {
           nextSequence();
         }, 1000);
       }
-
     } else {
-
-      console.log("wrong");
-
       playSound("wrong");
-
       $("body").addClass("game-over");
       setTimeout(function () {
         $("body").removeClass("game-over");
       }, 200);
 
-      $("#level-title").text("Game Over, Press Any Key to Restart");
+      $("#title").text("Game Over, Press Any Key to Restart");
       startOver();
     }
 
 }
 
 function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColour = buttonColours[randomNumber];
-        gamePattern.push(randomChosenColour);
-        $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    var randomChosenColor = buttonColors[randomNumber];
+        gamePattern.push(randomChosenColor);
+        
+        $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
-        $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+        $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
-        var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+        var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
         audio.play();  
         
-    playSound(randomChosenColour);   
+    playSound(randomChosenColor);   
 }
 
 function playSound(name) {
