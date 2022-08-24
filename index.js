50ed95b2199a0ff6e88643c398a6c22a103efedc
Anders Pihlblad <anderspihlblad1998@gmail.com>
2022-08-24 23:51:40 +0000

    added commets and made code neat

--- index.js
+++ index.js
@@ -18,15 +18,14 @@ $(".btn").click(function() {
  
     let userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
-
-    playSound(userChosenColour);
+    
     animatePress(userChosenColour);
 
     checkAnswer(userClickedPattern.length-1);
   
   });
 
-  function checkAnswer(currentLevel) {
+function checkAnswer(currentLevel) {
 
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
       if (userClickedPattern.length === gamePattern.length){
@@ -35,7 +34,6 @@ $(".btn").click(function() {
         }, 1000);
       }
     } else {
-      playSound("wrong");
       $("body").addClass("game-over");
       setTimeout(function () {
         $("body").removeClass("game-over");
@@ -55,14 +53,8 @@ function nextSequence() {
     var randomChosenColour = buttonColours[randomNumber];
         gamePattern.push(randomChosenColour);
         
-        $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);  
-        playSound(randomChosenColour);   
+        $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);    
 }
-
-function playSound(name) {
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
-  }
 
 function animatePress(currentColour) { 
     $("#" + currentColour).addClass("pressed");
