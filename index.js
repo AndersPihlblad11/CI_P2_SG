08fb1e61bc1e783722d7f9aba3b3602f7cdb7989
Anders Pihlblad <anderspihlblad1998@gmail.com>
2022-08-24 11:57:15 +0000

    used jquery to detect when any of the buttons are clicked

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 let buttonColors= ["red", "blue", "green", "yellow"];
 
 let gamePattern= [];
-var userClickedPattern = [];
 
-var started = false;
-var level = 0;
+let userClickedPattern = [];
 
+$(".btn").click(function() {
+
+  
+  });
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
         gamePattern.push(randomChosenColour);
+        $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 }
